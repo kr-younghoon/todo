f42e49028984d2400bc532d47afd89c7eccf9ea4
이영훈 <sullanta0802@gmail.com>
2025-05-09 02:12:03 +0900

    feat: item 상세 페이지 라우트 추가 및 초기 메인 페이지 정리

---
+++ src/app/items/[itemid]/page.tsx
@@ -0,0 +1,22 @@
+export default function item() {
+    return (
+        <>
+            <div>
+                <button></button>
+                <h1>head</h1>
+            </div>
+            <div>
+                <div>
+                    <p>이미지</p>
+                </div>
+                <div>
+                    <p>메모</p>
+                </div>
+            </div>
+            <div>
+                <button>수정 완료</button>
+                <button>삭제하기</button>
+            </div>
+        </>
+    );
+}
