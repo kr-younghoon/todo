05d003f8014400dcee6f2fc3ba14a1809e4e97b5
이영훈 <sullanta0802@gmail.com>
2025-05-10 15:25:16 +0900

    feat: 헤더 컴포넌트 추가 및 스타일 정의

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+import styles from './styles/Header.module.css';
+import ImgHeaderLogoLarge from './imgs/img-header-logo-large';
+import ImgHeaderLogoSmall from './imgs/img-header-logo-small';
+
 export default function Header() {
     return (
         <>
-            <h1>do it ;</h1>
+            <header className={styles.header}>
+                <Link href="/">
+                    <ImgHeaderLogoLarge
+                        className={styles.iconLarge}
+                    />
+                    <ImgHeaderLogoSmall
+                        className={styles.iconSmall}
+                    />
+                </Link>
+            </header>
         </>
     );
 }
